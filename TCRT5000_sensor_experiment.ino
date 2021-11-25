a845f3cba14d461624cdfd2b35bb21baf506721c
Eimantas Kozeniauskas <37850648+Eimpau@users.noreply.github.com>
2021-11-25 16:25:13 +0000

    Add files via upload
    Added arduino file

---
+++ TCRT5000_sensor_experiment.ino
@@ -0,0 +1,28 @@
+int sensor_value_pt;
+int sensor_value_led;
+float voltage_pt;
+float voltage_led;
+
+void setup() 
+{
+Serial.begin(9600);  
+}
+
+void loop() 
+{
+sensor_value_pt = analogRead(A3); 
+voltage_pt = sensor_value_pt * (5.0 / 1023.0);
+Serial.print("Phototransistor Voltage: ");
+Serial.println(voltage_pt);
+
+delay(1);
+
+sensor_value_led = analogRead(A2);
+voltage_led = sensor_value_led * (5.0 / 1023);
+Serial.print("LED Voltage: ");
+Serial.println(voltage_led);
+
+Serial.println();
+
+delay(500);
+}     
