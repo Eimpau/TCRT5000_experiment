9aeae30d07862a767fb26d17177a4f7fe78f7d69
Eimantas Kozeniauskas <37850648+Eimpau@users.noreply.github.com>
2021-11-25 17:29:57 +0000

    Requires Current Monitoring
    Added current calculation. Needs testing.

--- TCRT5000_sensor_experiment.ino
+++ TCRT5000_sensor_experiment.ino
@@ -2,6 +2,7 @@ int sensor_value_pt;
 int sensor_value_led;
 float voltage_pt;
 float voltage_led;
+float current_led;
 
 void setup() 
 {
@@ -21,6 +22,8 @@ sensor_value_led = analogRead(A2);
 voltage_led = sensor_value_led * (5.0 / 1023);
 Serial.print("LED Voltage: ");
 Serial.println(voltage_led);
+current_led = voltage_led / 100;
+Serial.println(current_led);
 
 Serial.println();
 
