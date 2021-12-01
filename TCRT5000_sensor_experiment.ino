883d54764e08dea587d02de3a76b09c54ec6f3b5
Eimantas Kozeniauskas <37850648+Eimpau@users.noreply.github.com>
2021-12-01 21:08:13 +0000

    Add files via upload
    Updated code to measure current and voltage of both the LED and Phototransistor of the TCRT5000.

--- TCRT5000_sensor_experiment.ino
+++ TCRT5000_sensor_experiment.ino
@@ -2,6 +2,7 @@ int sensor_value_pt;
 int sensor_value_led;
 float voltage_pt;
 float voltage_led;
+float current_pt;
 float current_led;
 
 void setup() 
@@ -11,21 +12,29 @@ Serial.begin(9600);
 
 void loop() 
 {
-sensor_value_pt = analogRead(A3); 
-voltage_pt = sensor_value_pt * (5.0 / 1023.0);
-Serial.print("Phototransistor Voltage: ");
-Serial.println(voltage_pt);
+  sensor_value_pt = analogRead(A3); 
 
-delay(1);
+  voltage_pt = sensor_value_pt * (5.0 / 1023.0);
+  Serial.print("Phototransistor Voltage: ");
+  Serial.println(voltage_pt);
 
-sensor_value_led = analogRead(A2);
-voltage_led = sensor_value_led * (5.0 / 1023);
-Serial.print("LED Voltage: ");
-Serial.println(voltage_led);
-current_led = voltage_led / 100;
-Serial.println(current_led);
+  current_pt = (5.0 - voltage_pt) / 15000;
+  Serial.print("Phototransistor Current: ");
+  Serial.println(current_pt, 6);
 
-Serial.println();
+  delay(1);
 
-delay(500);
+  sensor_value_led = analogRead(A2);
+
+  voltage_led = sensor_value_led * (5.0 / 1023);
+  Serial.print("LED Voltage: ");
+  Serial.println(voltage_led);
+
+  current_led = (5.0 - voltage_led) / 170;
+  Serial.print("Led Current: ");
+  Serial.println(current_led, 3);
+
+  Serial.println();
+
+  delay(1000);
 }     
